6032a7b86680274736b345eaf368360a333a9be9
KarthusLorin <karthuslorin@gmail.com>
2018-11-04 01:26:09 +0800

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,17 +6,19 @@
     <el-main>
       <nuxt/>
     </el-main>
-    <el-footer height="100%">Footer</el-footer>
+    <el-footer height="auto">
+      <MyFooter />
+    </el-footer>
   </el-container>
 </template>
 
 <script>
   import MyHeader from '~/components/public/header'
-
+  import MyFooter from '~/components/public/footer'
   export default {
-    components: {
-      MyHeader
+    components:{
+      MyHeader,
+      MyFooter
     }
   }
-
 </script>
