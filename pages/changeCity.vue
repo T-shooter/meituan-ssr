b18e6e5604dff908ace62d0e3befbc31c6eaa7f0
KarthusLorin <karthuslorin@gmail.com>
2018-11-09 23:16:35 +0800

    add changeCity

---
+++ pages/changeCity.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24">
+        <iSelect/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <Hot/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import iSelect from '~/components/changeCity/iselect'
+  import Hot from '~/components/changeCity/hot.vue'
+
+  export default {
+    components: {
+      iSelect,
+      Hot
+    }
+  }
+</script>
+
+<style>
+
+</style>
